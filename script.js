0d3f80a821d4d85eededb5517f7c64768859bf88
Luca Bonassi <luca.bonassi@studenti.liceomascheroni.it>
2026-03-22 21:18:56 +0100

    commit

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ window.addEventListener('mousemove', (e) => {
     document.body.style.setProperty('--mouse-x', `${x}%`);
     document.body.style.setProperty('--mouse-y', `${y}%`);
 });
+
+window.addEventListener('mousemove', (e) => {
+    const x = Math.round((e.clientX / window.innerWidth) * 100);
+    const y = Math.round((e.clientY / window.innerHeight) * 100);
+    document.body.style.setProperty('--mouse-x', x + '%');
+    document.body.style.setProperty('--mouse-y', y + '%');
+});
+
+const btn = document.querySelector('.fx-liquid');
+
+btn.addEventListener('mousedown', () => {
+    btn.style.transform = 'translate(-50%, 2px) scale(0.95)';
+});
+
+btn.addEventListener('mouseup', () => {
+    btn.style.transform = 'translate(-50%, 0) scale(1)';
+});
